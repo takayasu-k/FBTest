60b130d44efa1d2748469b4c2bfc5191468d5d5f
takayasu-k <takayasu7468@gmail.com>
2018-07-07 20:18:30 +0900

    Initial Commit

---
+++ FBTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FBTest
+//
+//  Created by taka on 2018/07/07.
+//  Copyright © 2018年 Takayasu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
